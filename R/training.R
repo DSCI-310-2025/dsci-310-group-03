ca561b46f47a55e2a129ed866045083033819149
Roy Oh <royoh3@gmail.com>
2025-04-03 20:54:47 -0700

    added examples - dontrun

--- R/training.R
+++ R/training.R
@@ -5,6 +5,16 @@
 #' @param train_data A dataframe containing the training data
 #' @return A trained multinomial model
 #' @export
+#' 
+#'
+#' @examples
+#' # example code
+#' 
+#' \dontrun{
+#' training_mlr_model(train_data)
+#' }
+#'
+#'
 training_mlr_model <- function(train_data) {
   library(nnet)
   model <- multinom(RiskLevel ~ ., data = train_data, trace = FALSE)
@@ -16,6 +26,14 @@ training_mlr_model <- function(train_data) {
 #' @param train_data A dataframe containing the training data
 #' @return A trained random forest model
 #' @export
+#' 
+#' 
+#' @examples
+# example code
+#' 
+#' \dontrun{
+#' training_rf_model(train_data)
+#' }
 training_rf_model <- function(train_data) {
   library(randomForest)
   model <- randomForest(RiskLevel ~ ., data = train_data, ntree = 500, importance = TRUE)
